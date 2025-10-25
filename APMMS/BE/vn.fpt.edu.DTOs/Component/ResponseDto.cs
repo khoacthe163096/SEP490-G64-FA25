619b73293fc9872a12872b62aaf22c5c8f2c48da
ThinhHE163873 <thinhthhe163873@fpt.edu.vn>
2025-10-26 00:06:27 +0700

    Update ResponseDto

--- APMMS/BE/vn.fpt.edu.DTOs/Component/ResponseDto.cs
+++ APMMS/BE/vn.fpt.edu.DTOs/Component/ResponseDto.cs
@@ -2,7 +2,15 @@ namespace BE.vn.fpt.edu.DTOs.Component
 {
     public class ResponseDto
     {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public decimal? UnitPrice { get; set; }
+        public int QuantityStock { get; set; }
+        public long? TypeComponentId { get; set; }
+        public string TypeComponentName { get; set; } // optional for include
+        public long? BranchId { get; set; }
+        public string ImageUrl { get; set; }
+        public bool IsActive { get; set; }
     }
 }
-
-
