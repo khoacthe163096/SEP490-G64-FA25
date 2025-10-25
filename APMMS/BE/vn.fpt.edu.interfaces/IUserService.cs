7feb30d3474a76edc2bb226ab1665c1bb87290e7
DESKTOP-V0KATD5\admin <datlthe160813@fpt.edu.vn>
2025-10-26 04:12:32 +0700

    Delete

--- APMMS/BE/vn.fpt.edu.interfaces/IUserService.cs
+++
@@ -1,8 +0,0 @@
-﻿using BE.vn.fpt.edu.models;
-
-public interface IUserService
-{
-    Task<User?> GetUserByEmailAsync(string email);
-    Task SaveResetTokenAsync(int userId, string token);
-    Task<bool> ResetPasswordAsync(string token, string newPassword);
-}
