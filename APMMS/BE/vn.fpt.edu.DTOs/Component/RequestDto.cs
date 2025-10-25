172f19286d6a2ee90c777498faee2e89636dfbc2
ThinhHE163873 <thinhthhe163873@fpt.edu.vn>
2025-10-26 00:05:43 +0700

    Update RequestDTO

--- APMMS/BE/vn.fpt.edu.DTOs/Component/RequestDto.cs
+++ APMMS/BE/vn.fpt.edu.DTOs/Component/RequestDto.cs
@@ -2,7 +2,12 @@ namespace BE.vn.fpt.edu.DTOs.Component
 {
     public class RequestDto
     {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public decimal? UnitPrice { get; set; }
+        public int? QuantityStock { get; set; }
+        public long? TypeComponentId { get; set; }
+        public long? BranchId { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
-
-
