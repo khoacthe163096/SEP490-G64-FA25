d4d4405aa02740e838b10514922f7ae0c63a555e
ThinhHE163873 <thinhthhe163873@fpt.edu.vn>
2025-10-26 00:12:24 +0700

    update field active

--- APMMS/BE/vn.fpt.edu.DTOs/Component/RequestDto.cs
+++ APMMS/BE/vn.fpt.edu.DTOs/Component/RequestDto.cs
@@ -9,5 +9,6 @@ namespace BE.vn.fpt.edu.DTOs.Component
         public long? TypeComponentId { get; set; }
         public long? BranchId { get; set; }
         public string ImageUrl { get; set; }
+        public bool IsActive { get; set; }
     }
 }
