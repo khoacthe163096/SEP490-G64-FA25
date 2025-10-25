bbe8288a4b6d65aba490fe98f250826a2e1a3dc3
ThinhHE163873 <thinhthhe163873@fpt.edu.vn>
2025-10-26 00:07:26 +0700

    Update interface for compo repo

--- APMMS/BE/vn.fpt.edu.repository/IRepository/IComponentRepository.cs
+++ APMMS/BE/vn.fpt.edu.repository/IRepository/IComponentRepository.cs
@@ -1,8 +1,15 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BE.vn.fpt.edu.models;
+
 namespace BE.vn.fpt.edu.repository.IRepository
 {
     public interface IComponentRepository
     {
+        Task<IEnumerable<Component>> GetAllAsync(bool onlyActive = false, long? typeComponentId = null, long? branchId = null);
+        Task<Component> GetByIdAsync(long id);
+        Task<Component> CreateAsync(Component entity);
+        Task UpdateAsync(Component entity);
+        Task SetActiveAsync(long id, bool isActive);
     }
 }
-
-
