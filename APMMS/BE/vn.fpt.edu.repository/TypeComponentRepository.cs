4b7f94e4fa41d33a1d53cfdae9a12407f5fbcb40
ThinhHE163873 <thinhthhe163873@fpt.edu.vn>
2025-10-25 22:53:14 +0700

    Add interfaces

--- APMMS/BE/vn.fpt.edu.repository/TypeComponentRepository.cs
+++ APMMS/BE/vn.fpt.edu.repository/TypeComponentRepository.cs
@@ -7,6 +7,14 @@ using BE.vn.fpt.edu.models;
 
 namespace BE.vn.fpt.edu.repository
 {
+    public interface ITypeComponentRepository
+    {
+        Task<IEnumerable<TypeComponent>> GetAllAsync(bool onlyActive = false);
+        Task<TypeComponent> GetByIdAsync(long id);
+        Task<TypeComponent> CreateAsync(TypeComponent entity);
+        Task UpdateAsync(TypeComponent entity);
+        Task SetActiveAsync(long id, bool isActive);
+    }
     public class TypeComponentRepository : ITypeComponentRepository
     {
     }
