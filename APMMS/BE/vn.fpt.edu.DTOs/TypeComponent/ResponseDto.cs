db7acf09c6c42da9cd31903713452b96df010078
ThinhHE163873 <thinhthhe163873@fpt.edu.vn>
2025-10-27 23:07:37 +0700

    update response dto

--- APMMS/BE/vn.fpt.edu.DTOs/TypeComponent/ResponseDto.cs
+++ APMMS/BE/vn.fpt.edu.DTOs/TypeComponent/ResponseDto.cs
@@ -3,8 +3,13 @@ namespace BE.vn.fpt.edu.DTOs.TypeComponent
     public class ResponseDto
     {
         public long Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public bool IsActive { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Description { get; set; }
+
+        public long? BranchId { get; set; }
+        public string? BranchName { get; set; }
+
+        public string? StatusCode { get; set; }
+        public string? StatusName { get; set; } // from StatusLookup table
     }
 }
