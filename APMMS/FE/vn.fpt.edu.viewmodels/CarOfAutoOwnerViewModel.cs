1b900d305077122773ea32b53f0ab5bf6e68bdc2
Pham Dinh Que <quepdhs170849@fpt.edu.vn>
2025-11-02 15:33:18 +0700

    FE Owner

---
+++ APMMS/FE/vn.fpt.edu.viewmodels/CarOfAutoOwnerViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FE.vn.fpt.edu.viewmodels
+{
+    public class CarOfAutoOwnerViewModel
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("userId")]
+        public int UserId { get; set; }
+
+        [JsonPropertyName("carName")]
+        public string? CarName { get; set; }
+
+        [JsonPropertyName("carModel")]
+        public string? CarModel { get; set; }
+
+        [JsonPropertyName("vehicleTypeId")]
+        public int? VehicleTypeId { get; set; }
+
+        [JsonPropertyName("color")]
+        public string? Color { get; set; }
+
+        [JsonPropertyName("licensePlate")]
+        public string? LicensePlate { get; set; }
+
+        [JsonPropertyName("vehicleEngineNumber")]
+        public string? VehicleEngineNumber { get; set; }
+
+        [JsonPropertyName("vinNumber")]
+        public string? VinNumber { get; set; }
+
+        [JsonPropertyName("yearOfManufacture")]
+        public int? YearOfManufacture { get; set; }
+
+        [JsonPropertyName("branchId")]
+        public int? BranchId { get; set; }
+
+        [JsonPropertyName("createdDate")]
+        public DateTime? CreatedDate { get; set; }
+
+        [JsonPropertyName("lastModifiedDate")]
+        public DateTime? LastModifiedDate { get; set; }
+    }
+}
