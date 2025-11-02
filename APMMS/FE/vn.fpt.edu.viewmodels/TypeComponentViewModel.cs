4cde96203806c3da7b48ba805724763a9d55e32d
ThinhHE163873 <thinhthhe163873@fpt.edu.vn>
2025-11-02 22:30:18 +0700

    add base code for typecompo vm

--- APMMS/FE/vn.fpt.edu.viewmodels/TypeComponentViewModel.cs
+++ APMMS/FE/vn.fpt.edu.viewmodels/TypeComponentViewModel.cs
@@ -2,5 +2,18 @@
 {
     public class TypeComponentViewModel
     {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public long? BranchId { get; set; }
+        public string? StatusCode { get; set; }
+    }
+
+    public class TypeComponentIndexViewModel
+    {
+        public IEnumerable<TypeComponentViewModel> Items { get; set; } = new List<TypeComponentViewModel>();
+        public string? Search { get; set; }
+        public long? BranchId { get; set; }
+        public string? StatusCode { get; set; }
     }
 }
