cd8effbf18fb2c198d6061a3b613bb2c0912fcb9
ThinhHE163873 <thinhthhe163873@fpt.edu.vn>
2025-11-04 00:32:41 +0700

    update service interface

--- APMMS/BE/vn.fpt.edu.interfaces/ITypeComponentService.cs
+++ APMMS/BE/vn.fpt.edu.interfaces/ITypeComponentService.cs
@@ -1,15 +1,13 @@
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using BE.vn.fpt.edu.DTOs.TypeComponent;
 
 namespace BE.vn.fpt.edu.interfaces
 {
     public interface ITypeComponentService
     {
-        Task<IEnumerable<ResponseDto>> GetAllAsync(long? branchId = null, string? statusCode = null);
+        Task<IEnumerable<ResponseDto>> GetAllAsync();
         Task<ResponseDto?> GetByIdAsync(long id);
         Task<ResponseDto> CreateAsync(RequestDto dto);
-        Task<ResponseDto?> UpdateAsync(RequestDto dto);
-        Task DisableEnableAsync(long id, string statusCode);
+        Task<ResponseDto?> UpdateAsync(long id, RequestDto dto);
+        Task<bool> DeleteAsync(long id);
     }
 }
