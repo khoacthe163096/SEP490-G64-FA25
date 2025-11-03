184849d4eb713571de7d03f0b0882470c7aedeaf
ThinhHE163873 <thinhthhe163873@fpt.edu.vn>
2025-11-04 00:29:32 +0700

    update IRepository

--- APMMS/BE/vn.fpt.edu.repository/IRepository/ITypeComponentRepository.cs
+++ APMMS/BE/vn.fpt.edu.repository/IRepository/ITypeComponentRepository.cs
@@ -1,16 +1,13 @@
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using BE.vn.fpt.edu.models;
+using BE.models;
 
 namespace BE.vn.fpt.edu.repository.IRepository
 {
     public interface ITypeComponentRepository
     {
-        Task<IEnumerable<TypeComponent>> GetAllAsync(long? branchId = null, string? statusCode = null);
+        Task<IEnumerable<TypeComponent>> GetAllAsync();
         Task<TypeComponent?> GetByIdAsync(long id);
         Task<TypeComponent> AddAsync(TypeComponent entity);
         Task<TypeComponent> UpdateAsync(TypeComponent entity);
-        Task DisableEnableAsync(long id, string statusCode);
-        Task<bool> ExistsAsync(long id);
+        Task<bool> DeleteAsync(long id);
     }
 }
