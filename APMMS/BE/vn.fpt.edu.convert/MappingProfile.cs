5a9d15a6e9d62a6780df7e1ce23aeb7a92963ba9
ThinhHE163873 <thinhthhe163873@fpt.edu.vn>
2025-11-04 00:39:26 +0700

    DI regis typecompo

--- APMMS/BE/vn.fpt.edu.convert/MappingProfile.cs
+++ APMMS/BE/vn.fpt.edu.convert/MappingProfile.cs
@@ -1,21 +1,6 @@
 using AutoMapper;
-using BE.vn.fpt.edu.models;
-using BE.vn.fpt.edu.DTOs.Auth;
-using BE.vn.fpt.edu.DTOs.AutoOwner;
-using BE.vn.fpt.edu.DTOs.Component;
-using BE.vn.fpt.edu.DTOs.Employee;
-using BE.vn.fpt.edu.DTOs.Feedback;
-using BE.vn.fpt.edu.DTOs.HistoryLog;
-using BE.vn.fpt.edu.DTOs.MaintenanceTicket;
-using BE.vn.fpt.edu.DTOs.Profile;
-using BE.vn.fpt.edu.DTOs.Report;
-using BE.vn.fpt.edu.DTOs.Role;
-using BE.vn.fpt.edu.DTOs.ServicePackage;
-using BE.vn.fpt.edu.DTOs.ServiceSchedule;
-using BE.vn.fpt.edu.DTOs.ServiceTask;
-using BE.vn.fpt.edu.DTOs.TotalReceipt;
+using BE.models;
 using BE.vn.fpt.edu.DTOs.TypeComponent;
-using BE.vn.fpt.edu.DTOs.VehicleCheckin;
 
 namespace BE.vn.fpt.edu.convert
 {
@@ -23,96 +8,11 @@ namespace BE.vn.fpt.edu.convert
     {
         public MappingProfile()
         {
-            // Auth mappings removed - using database entities directly
-
-            // AutoOwner mappings
-            CreateMap<User, BE.vn.fpt.edu.DTOs.AutoOwner.ResponseDto>();
-            CreateMap<BE.vn.fpt.edu.DTOs.AutoOwner.RequestDto, User>();
-
-            // Component mappings
-            CreateMap<BE.vn.fpt.edu.DTOs.Component.RequestDto, Component>()
-                .ForMember(dest => dest.Id, opt => opt.Condition(src => src.Id.HasValue))
-                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id ?? 0L));
-
-            CreateMap<Component, BE.vn.fpt.edu.DTOs.Component.ResponseDto>()
-                .ForMember(dest => dest.TypeComponentName, opt => opt.Ignore())
-                .ForMember(dest => dest.BranchName, opt => opt.Ignore());
-
-
-            // Employee mappings
-            CreateMap<User, BE.vn.fpt.edu.DTOs.Employee.EmployeeResponseDto>();
-            CreateMap<BE.vn.fpt.edu.DTOs.Employee.EmployeeRequestDto, User>();
-
-            // Feedback mappings
-            CreateMap<Feedback, BE.vn.fpt.edu.DTOs.Feedback.ResponseDto>();
-            CreateMap<BE.vn.fpt.edu.DTOs.Feedback.RequestDto, Feedback>();
-
-            // HistoryLog mappings
-            CreateMap<HistoryLog, BE.vn.fpt.edu.DTOs.HistoryLog.ResponseDto>();
-            CreateMap<BE.vn.fpt.edu.DTOs.HistoryLog.RequestDto, HistoryLog>();
-
-            // MaintenanceTicket mappings
-            CreateMap<MaintenanceTicket, BE.vn.fpt.edu.DTOs.MaintenanceTicket.ResponseDto>();
-            CreateMap<MaintenanceTicket, BE.vn.fpt.edu.DTOs.MaintenanceTicket.ListResponseDto>()
-                .ForMember(dest => dest.CreatedDate, opt => opt.MapFrom(src => src.StartTime ?? DateTime.Now.AddDays(-src.Id)))
-                .ForMember(dest => dest.CarName, opt => opt.MapFrom(src => src.Car != null ? src.Car.CarName : null))
-                .ForMember(dest => dest.ConsulterName, opt => opt.MapFrom(src => src.Consulter != null ? $"{src.Consulter.FirstName} {src.Consulter.LastName}".Trim() : null))
-                .ForMember(dest => dest.TechnicianName, opt => opt.MapFrom(src => src.Technician != null ? $"{src.Technician.FirstName} {src.Technician.LastName}".Trim() : null))
+            CreateMap<TypeComponent, ResponseDto>()
                 .ForMember(dest => dest.BranchName, opt => opt.MapFrom(src => src.Branch != null ? src.Branch.Name : null))
-                .ForMember(dest => dest.CustomerName, opt => opt.MapFrom(src => src.Car != null && src.Car.User != null ? $"{src.Car.User.FirstName} {src.Car.User.LastName}".Trim() : null))
-                .ForMember(dest => dest.Code, opt => opt.MapFrom(src => src.Code));
-            CreateMap<BE.vn.fpt.edu.DTOs.MaintenanceTicket.RequestDto, MaintenanceTicket>();
+                .ForMember(dest => dest.StatusName, opt => opt.MapFrom(src => src.StatusCodeNavigation != null ? src.StatusCodeNavigation.Name : null));
 
-            // Profile mappings
-            CreateMap<User, BE.vn.fpt.edu.DTOs.Profile.ResponseDto>();
-            CreateMap<BE.vn.fpt.edu.DTOs.Profile.RequestDto, User>();
-
-            // Report mappings
-            CreateMap<TotalReceipt, BE.vn.fpt.edu.DTOs.Report.ResponseDto>();
-            CreateMap<BE.vn.fpt.edu.DTOs.Report.RequestDto, TotalReceipt>();
-
-            // Role mappings
-            CreateMap<Role, BE.vn.fpt.edu.DTOs.Role.ResponseDto>();
-            CreateMap<BE.vn.fpt.edu.DTOs.Role.RequestDto, Role>();
-
-            // ServicePackage mappings
-            CreateMap<BE.vn.fpt.edu.DTOs.ServicePackage.RequestDto, ServicePackage>()
-             .ForMember(dest => dest.Id, opt => opt.Condition(src => src.Id.HasValue))
-             .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id ?? 0L))
-             .ForMember(dest => dest.Components, opt => opt.Ignore()); // handle manually in service
-
-            CreateMap<ServicePackage, BE.vn.fpt.edu.DTOs.ServicePackage.ResponseDto>()
-                .ForMember(dest => dest.Components, opt => opt.Ignore()); // fill manually
-
-            // ServiceSchedule mappings
-            CreateMap<ScheduleService, BE.vn.fpt.edu.DTOs.ServiceSchedule.ResponseDto>();
-            CreateMap<BE.vn.fpt.edu.DTOs.ServiceSchedule.RequestDto, ScheduleService>();
-
-            // ServiceTask mappings
-            CreateMap<ServiceTask, BE.vn.fpt.edu.DTOs.ServiceTask.ServiceTaskResponseDto>();
-            CreateMap<ServiceTask, BE.vn.fpt.edu.DTOs.ServiceTask.ServiceTaskListResponseDto>();
-            CreateMap<BE.vn.fpt.edu.DTOs.ServiceTask.ServiceTaskRequestDto, ServiceTask>();
-            CreateMap<BE.vn.fpt.edu.DTOs.ServiceTask.ServiceTaskUpdateDto, ServiceTask>();
-
-            // TotalReceipt mappings
-            CreateMap<TotalReceipt, BE.vn.fpt.edu.DTOs.TotalReceipt.ResponseDto>();
-            CreateMap<BE.vn.fpt.edu.DTOs.TotalReceipt.RequestDto, TotalReceipt>();
-            
-            // TypeComponent mappings
-            CreateMap<BE.vn.fpt.edu.DTOs.TypeComponent.RequestDto, TypeComponent>()
-            .ForMember(dest => dest.Id, opt => opt.Condition(src => src.Id.HasValue))
-            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id ?? 0L));
-
-            CreateMap<TypeComponent, BE.vn.fpt.edu.DTOs.TypeComponent.ResponseDto>();
-
-            // VehicleCheckin mappings
-            CreateMap<VehicleCheckin, BE.vn.fpt.edu.DTOs.VehicleCheckin.ResponseDto>();
-            CreateMap<BE.vn.fpt.edu.DTOs.VehicleCheckin.VehicleCheckinRequestDto, VehicleCheckin>();
-
-            // CarOfOutoOwner mappings
-            CreateMap<Car, BE.vn.fpt.edu.DTOs.CarOfAutoOwner.ResponseDto>();
-            CreateMap<BE.vn.fpt.edu.DTOs.CarOfAutoOwner.RequestDto, Car>();
-
+            CreateMap<RequestDto, TypeComponent>();
         }
     }
 }
