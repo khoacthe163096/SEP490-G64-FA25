401506006d26a64c3d4bfe5f1f14bcab429bd51c
Khoact <khoacthe163096@fpt.edu.vn>
2025-11-07 20:08:26 +0700

    update Schedule repo

--- APMMS/BE/vn.fpt.edu.repository/IRepository/IServiceScheduleRepository.cs
+++ APMMS/BE/vn.fpt.edu.repository/IRepository/IServiceScheduleRepository.cs
@@ -1,8 +1,18 @@
+using BE.vn.fpt.edu.models;
+
 namespace BE.vn.fpt.edu.repository.IRepository
 {
     public interface IServiceScheduleRepository
     {
+        Task<ScheduleService> CreateAsync(ScheduleService scheduleService);
+        Task<ScheduleService?> GetByIdAsync(long id);
+        Task<List<ScheduleService>> GetAllAsync(int page = 1, int pageSize = 10);
+        Task<List<ScheduleService>> GetByUserIdAsync(long userId);
+        Task<List<ScheduleService>> GetByBranchIdAsync(long branchId);
+        Task<List<ScheduleService>> GetByStatusAsync(string statusCode);
+        Task<List<ScheduleService>> GetByDateRangeAsync(DateTime startDate, DateTime endDate);
+        Task<ScheduleService> UpdateAsync(ScheduleService scheduleService);
+        Task<bool> DeleteAsync(long id);
+        Task<bool> ExistsAsync(long id);
     }
 }
-
-
