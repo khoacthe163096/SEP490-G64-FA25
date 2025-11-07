001b2f61853e3bc909ffb065e9626c869d132981
Khoact <khoacthe163096@fpt.edu.vn>
2025-11-07 20:06:45 +0700

    Update Repositor

--- APMMS/BE/vn.fpt.edu.interfaces/IServiceScheduleService.cs
+++ APMMS/BE/vn.fpt.edu.interfaces/IServiceScheduleService.cs
@@ -1,8 +1,19 @@
+using BE.vn.fpt.edu.DTOs.ServiceSchedule;
+
 namespace BE.vn.fpt.edu.interfaces
 {
     public interface IServiceScheduleService
     {
+        Task<ResponseDto> CreateScheduleAsync(RequestDto request);
+        Task<ResponseDto> GetScheduleByIdAsync(long id);
+        Task<List<ListResponseDto>> GetAllSchedulesAsync(int page = 1, int pageSize = 10);
+        Task<List<ListResponseDto>> GetSchedulesByUserIdAsync(long userId);
+        Task<List<ListResponseDto>> GetSchedulesByBranchIdAsync(long branchId);
+        Task<List<ListResponseDto>> GetSchedulesByStatusAsync(string statusCode);
+        Task<List<ListResponseDto>> GetSchedulesByDateRangeAsync(DateTime startDate, DateTime endDate);
+        Task<ResponseDto> UpdateScheduleAsync(long id, UpdateScheduleDto request);
+        Task<ResponseDto> CancelScheduleAsync(long id, CancelScheduleDto? request = null);
+        Task<bool> DeleteScheduleAsync(long id);
+        Task<ResponseDto> CreatePublicBookingAsync(PublicBookingDto request);
     }
 }
-
-
