14611b2e06b76894abfa61e65c63f19f8b73ccba
TuyenNC <tuyennche161619@fpt.edu.vn>
2025-11-07 22:31:02 +0700

    Create ScheduleServiceNote

---
+++ APMMS/BE/vn.fpt.edu.models/ScheduleServiceNote.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BE.vn.fpt.edu.models;
+
+public partial class ScheduleServiceNote
+{
+    public long Id { get; set; }
+
+    public long ScheduleServiceId { get; set; }
+
+    public long ConsultantId { get; set; }
+
+    public string Note { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; }
+
+    public virtual User Consultant { get; set; } = null!;
+
+    public virtual ScheduleService ScheduleService { get; set; } = null!;
+}
