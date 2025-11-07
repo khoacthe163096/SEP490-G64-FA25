3c2c97829590a96d60c871a19a4f398910ee2a6d
Khoact <khoacthe163096@fpt.edu.vn>
2025-11-07 20:05:55 +0700

    update response

--- APMMS/BE/vn.fpt.edu.DTOs/ServiceSchedule/ResponseDto.cs
+++ APMMS/BE/vn.fpt.edu.DTOs/ServiceSchedule/ResponseDto.cs
@@ -2,7 +2,35 @@ namespace BE.vn.fpt.edu.DTOs.ServiceSchedule
 {
     public class ResponseDto
     {
+        public long Id { get; set; }
+        public long? UserId { get; set; }
+        public string? UserName { get; set; }
+        public string? UserEmail { get; set; }
+        public string? UserPhone { get; set; }
+        public long? CarId { get; set; }
+        public string? CarName { get; set; }
+        public string? LicensePlate { get; set; }
+        public string? CarModel { get; set; }
+        public DateTime ScheduledDate { get; set; }
+        public string? StatusCode { get; set; }
+        public string? StatusName { get; set; }
+        public long? BranchId { get; set; }
+        public string? BranchName { get; set; }
+        public string? BranchPhone { get; set; }
+    }
+
+    public class ListResponseDto
+    {
+        public long Id { get; set; }
+        public long? UserId { get; set; }
+        public string? UserName { get; set; }
+        public long? CarId { get; set; }
+        public string? CarName { get; set; }
+        public string? LicensePlate { get; set; }
+        public DateTime ScheduledDate { get; set; }
+        public string? StatusCode { get; set; }
+        public string? StatusName { get; set; }
+        public long? BranchId { get; set; }
+        public string? BranchName { get; set; }
     }
 }
-
-
