d16680da656cf01a40aabf26806cca390b22016f
TuyenNC <tuyennche161619@fpt.edu.vn>
2025-11-08 05:15:06 +0700

    Update repo

---
+++ APMMS/BE/vn.fpt.edu.models/ServiceCategory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BE.vn.fpt.edu.models;
+
+public partial class ServiceCategory
+{
+    public long Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public string? StatusCode { get; set; }
+
+    public virtual ICollection<MaintenanceTicket> MaintenanceTickets { get; set; } = new List<MaintenanceTicket>();
+
+    public virtual ICollection<ScheduleService> ScheduleServices { get; set; } = new List<ScheduleService>();
+
+    public virtual StatusLookup? StatusCodeNavigation { get; set; }
+}
