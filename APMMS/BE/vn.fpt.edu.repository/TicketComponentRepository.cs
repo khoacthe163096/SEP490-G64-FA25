ecd02cdbcff0a8475829630b0c8f4c9131d530bf
Khoact <khoacthe163096@fpt.edu.vn>
2025-11-10 17:39:04 +0700

    Create folder TicketComponent repo

---
+++ APMMS/BE/vn.fpt.edu.repository/TicketComponentRepository.cs
@@ -0,0 +1,74 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BE.vn.fpt.edu.models;
+using BE.vn.fpt.edu.repository.IRepository;
+
+namespace BE.vn.fpt.edu.repository
+{
+    public class TicketComponentRepository : ITicketComponentRepository
+    {
+        private readonly CarMaintenanceDbContext _context;
+
+        public TicketComponentRepository(CarMaintenanceDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<TicketComponent> AddAsync(TicketComponent entity)
+        {
+            var added = await _context.TicketComponents.AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return added.Entity;
+        }
+
+        public async Task<TicketComponent?> GetByIdAsync(long id)
+        {
+            return await _context.TicketComponents
+                .Include(tc => tc.Component)
+                    .ThenInclude(c => c!.TypeComponent)
+                .Include(tc => tc.MaintenanceTicket)
+                .FirstOrDefaultAsync(tc => tc.Id == id);
+        }
+
+        public async Task<IEnumerable<TicketComponent>> GetByMaintenanceTicketIdAsync(long maintenanceTicketId)
+        {
+            return await _context.TicketComponents
+                .Include(tc => tc.Component)
+                    .ThenInclude(c => c!.TypeComponent)
+                .Where(tc => tc.MaintenanceTicketId == maintenanceTicketId)
+                .ToListAsync();
+        }
+
+        public async Task<TicketComponent?> UpdateAsync(TicketComponent entity)
+        {
+            var existing = await _context.TicketComponents.FindAsync(entity.Id);
+            if (existing == null) return null;
+
+            existing.ComponentId = entity.ComponentId;
+            existing.Quantity = entity.Quantity;
+            existing.UnitPrice = entity.UnitPrice;
+
+            _context.TicketComponents.Update(existing);
+            await _context.SaveChangesAsync();
+            return existing;
+        }
+
+        public async Task<bool> DeleteAsync(long id)
+        {
+            var entity = await _context.TicketComponents.FindAsync(id);
+            if (entity == null) return false;
+
+            _context.TicketComponents.Remove(entity);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> ExistsAsync(long id)
+        {
+            return await _context.TicketComponents.AnyAsync(x => x.Id == id);
+        }
+    }
+}
+
