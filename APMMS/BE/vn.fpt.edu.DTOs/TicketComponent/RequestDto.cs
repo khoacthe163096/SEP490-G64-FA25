ecb2762d48df40762940f3f00bd363746f6e771f
Khoact <khoacthe163096@fpt.edu.vn>
2025-11-10 17:38:27 +0700

    Create folder TicketComponent request

---
+++ APMMS/BE/vn.fpt.edu.DTOs/TicketComponent/RequestDto.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BE.vn.fpt.edu.DTOs.TicketComponent
+{
+    public class RequestDto
+    {
+        public long? Id { get; set; }
+
+        [Required(ErrorMessage = "Maintenance Ticket ID is required")]
+        public long MaintenanceTicketId { get; set; }
+
+        [Required(ErrorMessage = "Component ID is required")]
+        public long ComponentId { get; set; }
+
+        [Required(ErrorMessage = "Quantity is required")]
+        [Range(1, int.MaxValue, ErrorMessage = "Quantity must be greater than 0")]
+        public int Quantity { get; set; }
+
+        [Range(0, double.MaxValue, ErrorMessage = "Unit price must be greater than or equal to 0")]
+        public decimal? UnitPrice { get; set; }
+    }
+}
+
