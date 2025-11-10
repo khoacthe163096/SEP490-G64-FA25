47c073a5bd6391e3f4cda1f486ec1398d8b61362
Khoact <khoacthe163096@fpt.edu.vn>
2025-11-10 17:38:39 +0700

    Create folder TicketComponent response

---
+++ APMMS/BE/vn.fpt.edu.DTOs/TicketComponent/ResponseDto.cs
@@ -0,0 +1,19 @@
+namespace BE.vn.fpt.edu.DTOs.TicketComponent
+{
+    public class ResponseDto
+    {
+        public long Id { get; set; }
+        public long MaintenanceTicketId { get; set; }
+        public long ComponentId { get; set; }
+        public int Quantity { get; set; }
+        public decimal? UnitPrice { get; set; }
+        public decimal? TotalPrice { get; set; }
+
+        // Component info
+        public string? ComponentName { get; set; }
+        public string? ComponentCode { get; set; }
+        public string? ComponentImageUrl { get; set; }
+        public string? TypeComponentName { get; set; }
+    }
+}
+
