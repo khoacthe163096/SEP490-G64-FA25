789976e507bf98d616ffa82682a1b06dcf34c10d
Khoact <khoacthe163096@fpt.edu.vn>
2025-11-12 22:50:39 +0700

    Update thanh toán

--- APMMS/FE/vn.fpt.edu.controllers/TotalReceiptController.cs
+++ APMMS/FE/vn.fpt.edu.controllers/TotalReceiptController.cs
@@ -1,15 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace FE.vn.fpt.edu.controllers
 {
     [Route("TotalReceipts")]
     public class TotalReceiptController : Controller
     {
+        private readonly IConfiguration _configuration;
+
+        public TotalReceiptController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         [HttpGet]
         [Route("")]
         public IActionResult Index()
         {
-			return View("~/vn.fpt.edu.views/TotalReceipts/Index.cshtml");
+            ViewBag.ApiBaseUrl = _configuration["ApiSettings:BaseUrl"] ?? "https://localhost:7173/api";
+            return View("~/vn.fpt.edu.views/TotalReceipts/Index.cshtml");
+        }
+
+        [HttpGet]
+        [Route("Details/{id}")]
+        public IActionResult Details(long id)
+        {
+            ViewBag.TotalReceiptId = id;
+            ViewBag.ApiBaseUrl = _configuration["ApiSettings:BaseUrl"] ?? "https://localhost:7173/api";
+            return View("~/vn.fpt.edu.views/TotalReceipts/Details.cshtml");
         }
     }
 }
