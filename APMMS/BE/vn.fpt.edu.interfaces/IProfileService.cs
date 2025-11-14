83e91a8be3f917b847294f0c8657db4364ed0849
datlt <Datlthe160813@fpt.edu.vn>
2025-11-15 01:20:04 +0700

    Update Profile Employee

--- APMMS/BE/vn.fpt.edu.interfaces/IProfileService.cs
+++ APMMS/BE/vn.fpt.edu.interfaces/IProfileService.cs
@@ -1,7 +1,13 @@
+using BE.vn.fpt.edu.DTOs.Employee;
+using Microsoft.AspNetCore.Http;
+
 namespace BE.vn.fpt.edu.interfaces
 {
     public interface IProfileService
     {
+        Task<EmployeeResponseDto?> GetMyProfileAsync(long userId);
+        Task<EmployeeResponseDto> UpdateMyProfileAsync(long userId, EmployeeProfileUpdateDto dto);
+        Task<string> UploadAvatarAsync(long userId, IFormFile file);
     }
 }
 
